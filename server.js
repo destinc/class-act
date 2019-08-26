5cb10a482dca60fde7c38aadbdfb1bc49a1d5621
Mattpheus <mhseecha@gmail.com>
2019-08-26 19:38:07 -0400

    heroky possible final fix

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const { Courses, Reviews, User } = require('./database/models')
 const PORT = process.env.PORT || 3001
 
 const app = express()
-
+app.use(cors())
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use(logger('dev'))
-app.use(cors())
+
 app.use(parser.json())
 app.use(express.urlencoded({ extended: false }))
 
