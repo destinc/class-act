eda2cf31f043e2dc39c03b3c452b6e5b4f28a3e6
Mattpheus <mhseecha@gmail.com>
2019-08-26 18:52:19 -0400

    fix typo

--- database/models.js
+++ database/models.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const bcrypt = require('bcrypt')
-const db = new Sequelize((process.env.DATABASE_URL, 'postgres://localhost:5432/courses_db'),{
+
+const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/courses_db'),{
     database: 'courses_db',
     dialect: 'postgres'
 })
